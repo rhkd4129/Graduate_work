228b6f791afe4b57ac5e72abb57b7171499b5061
Lee <rhkd4129@naver.com>
2022-11-30 08:50:23 +0900

    hahah

--- tkinter_1.py
+++ tkinter_1.py
@@ -40,7 +40,7 @@ def Clear():
     for w in main.place_slaves():
         w.destroy()
 
-#######################################################################
+##########################################################################
 # 기본 메인 창
 def mainWindow():
     global  search_image_entry
@@ -52,31 +52,23 @@ def mainWindow():
     # 폰트와 픽셀 크기 
     main.title("window")
     # 창 제목
-
-
 ################ <---- 첫쨰줄 ---->  #################
 
-    search_image_lbl = Label(main)
-    search_image_lbl.config(text = "search_image")
+    search_image_lbl = Label(main,text='search_image')
     search_image_lbl.place(x = 400, y= 80)
     # 레이블 상입과 안에  들어강 내용과 위치 
-
     search_image_entry = Entry(main)
     search_image_entry.place(x = 40, y= 80)
-    # entry 삽입
-    
-    
+    # entry 삽입(입력하는 칸)
 ################ <---- 두번째 줄 ---->  ###############
-    number_lbl = Label(main)
-    number_lbl.config(text = "number")
-    number_lbl.place(x = 400, y= 150)
 
+    number_lbl = Label(main,text='number')
+    number_lbl.place(x = 400, y= 150)
     number_entry = Entry(main)
     number_entry.place(x = 40, y= 150)
     
 ################ <---- 세번째 줄 ---->  ###############
-    GObtn = Button(main)
-    GObtn.config(text="GO")
+    GObtn = Button(main,text='GO')
     GObtn.place(x = 400, y=250)
     GObtn.config(command = GOClick)
 
@@ -100,8 +92,7 @@ def GOClick():
     ################################################
 
     if search_image_name in duplication_words_dict.keys():
-        lbl_1 = Label(main)
-        lbl_1.config(text ='어떤 단어가 맞나요?')
+        lbl_1 = Label(main,text='어떤 단어가 맞나요')
         lbl_1.place(x = 200, y= 250)
         main.geometry("700x400")
         main.option_add("*Font","맑은고딕 15")
