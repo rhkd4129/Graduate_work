e48096f7b48f676a11c966e8355b2126b0879c6a
Lee <rhkd4129@naver.com>
2023-03-17 22:34:13 +0900

    'django_setting'

--- graduateWork_site/graduateWork_site/urls.py
+++ graduateWork_site/graduateWork_site/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('image_site',include('image_site.urls')),
 
 ]
+
+if settings.DEBUG:#미디어파일에 대한 스태틱 서브기능?
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- graduateWork_site/image_site/models.py
+++ graduateWork_site/image_site/models.py
@@ -5,4 +5,8 @@ class Post(models.Model):
     search_keyword = models.CharField(max_length=15)
     image_number = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
-    images = models.ImageField(blank = True)#upload_to = 'blog/post/%Y/%m/%d'
+    images = models.ImageField(blank = True,upload_to='image/post')#upload_to = 'blog/post/%Y/%m/%d'
+
+    def __str__(self):
+        #return f"Post({self.id})"
+        return self.search_keyword
