185f9d02030a6ca27b7b5504ab03d6b0f28eb147
Lee <rhkd4129@naver.com>
2023-04-05 00:14:46 +0900

    최적화2

--- django_project_1-main/qwer/views.py
+++ django_project_1-main/qwer/views.py
@@ -36,27 +36,3 @@ def result(request,advice_pk):
     context = {'advice': advice, 'adviceimage': adviceimage}
     return render(request, 'qwer/result.html',context)
 
-
-
-# def search_image(request):
-#     if request.method =='POST':
-#         form  = searchForm(request.POST,request.FILES)
-#         if form.is_valid():
-#             advice = form.save(commit=False)
-#             keyword = form.cleaned_data['keyword']
-#             find_image_number = form.cleaned_data['find_image_number']
-#             keyword,path_list,file_name_list= craw(keyword,find_image_number)
-#             adivces=[]
-#             # for i in range(find_image_number):
-#                 # with open(path_list[i]+file_name_list[i], 'rb') as f:
-#                     # advice.searh_result_image.save(file_name_list[i], File(f), save=False)                                                                      
-#                 # adivces[i] = Advice.objects.create(customer='a',keyword=keyword,find_image_number=i,searh_result_image=File(f))                    
-#             adivces1= Advice.objects.create(customer='a',keyword=keyword,find_image_number=1,searh_result_image=path_list[0]+file_name_list[0])   
-#             adivces2= Advice.objects.create(customer='a',keyword=keyword,find_image_number=2,searh_result_image=path_list[1]+file_name_list[1])                                     
-#             advice_list = Advice.objects.all()
-
-            
-#             return render(request,'crawling/search_image.html',{'advice_list':advice_list})          
-#     else:
-#         form = searchForm()
-#     return render(request,'crawling/search_image.html',{'form':form})
