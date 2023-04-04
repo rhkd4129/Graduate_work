abf641095e20c1d8689e9f56c17616f99dd5d249
Lee <rhkd4129@naver.com>
2023-04-05 00:37:54 +0900

    clear

--- django_project_1-main/qwer/urls.py
+++ django_project_1-main/qwer/urls.py
@@ -8,7 +8,9 @@ app_name = 'qwer'
 urlpatterns=[
 
      path('',views.qwer,name = 'qwer'),
-     path('result/<int:advice_pk>/',views.result,name= 'result')
+     path('result/<int:advice_pk>/',views.result,name= 'result'),
+     path('trans_result/<int:advice_pk>/',views.trans_image_result,name= 'trans_image_result')
+
     #  path('search_image/',views.search_image, name = 'search_image'),
     # path(),
 ]
--- django_project_1-main/qwer/views.py
+++ django_project_1-main/qwer/views.py
@@ -6,8 +6,7 @@ from .image_db_save import craw
 from django.shortcuts import get_object_or_404
 from django.core.files import File
 
-from io import BytesIO
-# Image.open(fp)
+
 def qwer(request):
     if request.method =='POST':
         form  = searchForm(request.POST,request.FILES)
@@ -22,7 +21,6 @@ def qwer(request):
                 advice_image.image.save(f'image{i}.jpg', File(image_data),save=False)
                 advice_image.save()
                 i+=1
-            
             return redirect(advice)
     else:
         form = searchForm()
@@ -31,8 +29,13 @@ def qwer(request):
 def result(request,advice_pk):
     advice = get_object_or_404(Advice,id = advice_pk)#id로써도되고pk로써도된다? ,,.?
     adviceimage = AdviceImage.objects.filter(advice_id = advice_pk)
-    print(adviceimage)
-    print(advice)
     context = {'advice': advice, 'adviceimage': adviceimage}
     return render(request, 'qwer/result.html',context)
+
+
+def trans_image_result(request,advice_pk):
+    advice = get_object_or_404(Advice,id = advice_pk)#id로써도되고pk로써도된다? ,,.?
+    adviceimage = AdviceImage.objects.filter(advice_id = advice_pk)
+    context = {'advice': advice, 'adviceimage': adviceimage}
+    return render(request,'qwer/trans_image_result.html',context)
 
