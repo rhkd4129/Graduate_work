7f9ed76fd1e4c5c396bb6e9df0035d15d7693184
Lee <rhkd4129@naver.com>
2023-04-08 02:34:05 +0900

    '사람이죽어'

--- graduation_work/graduation_work_site/views.py
+++ graduation_work/graduation_work_site/views.py
@@ -3,12 +3,3 @@ from django.views.generic import TemplateView
 
 class TView(TemplateView):
     template_name = 'home.html'
-
-# def crawing(request):
-#     if request.method =='POST':
-#         form  = searchForm(request.POST)
-#         if form.is_valid():
-#             return redirect(advice)
-#     else:
-#         form = searchForm()
-#     return render(request,'coloring/search.html',{'form':form})
